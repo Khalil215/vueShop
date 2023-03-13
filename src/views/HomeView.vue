5f2b69db6c348964c912f98744291c2a51aa01f5
Khalil215 <ibrahimhamman215@gmail.com>
2023-03-13 21:49:03 +0100

    setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p className="text-red-500 font-bold">Hello mithereasdglhsg
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
