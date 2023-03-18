8d9f222e551241ef269d3f73096c0aab92c1865b
Khalil215 <ibrahimhamman215@gmail.com>
2023-03-18 22:09:14 +0100

    notfound

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-Not found 404
+  <div class="grid place-content-center w-[100vw] h-[100vh]">
+    <h1 class="text-2xl font-bold">Oopss... Wrong Route</h1>
+    <router-link :to="{name:'home'}"><div class="text-theme underline text-center">Return to Home page</div></router-link>
   </div>
 </template>
 
 <script setup>
+import { onMounted, onUnmounted } from "vue";
+import { useStore } from "vuex";
+const store = useStore();
 
+onMounted(() => {
+  store.commit("toggleShowNav", false);
+});
+onUnmounted(() => {
+  store.commit("toggleShowNav", true);
+});
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
